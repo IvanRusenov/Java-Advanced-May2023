c7526ac998ef67ea7e911f1d2bd7b38963a7bdff
IvanRusenov <i_rusenov@abv.bg>
2023-06-11 08:50:24 +0200

    Create A01CarInfo.java

---
+++ DefiningClassesLab/A01CarInfo/A01CarInfo.java
@@ -0,0 +1,19 @@
+package DefiningClassesLab.A01CarInfo;
+
+public class A01CarInfo {
+
+
+
+    public static void main(String[] args) {
+
+        int hash = "лада самара".hashCode();
+        System.out.println(hash);
+        int hash2 = "лада калина".hashCode();
+        System.out.println(hash == hash2);
+
+
+    }
+
+
+
+}
