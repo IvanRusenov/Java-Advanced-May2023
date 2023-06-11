1e1841f4167689baa19b09c00d323c155a8d5457
IvanRusenov <i_rusenov@abv.bg>
2023-06-11 08:51:02 +0200

    Create Car.java

---
+++ DefiningClassesLab/A01CarInfo/Car.java
@@ -0,0 +1,44 @@
+package DefiningClassesLab.A01CarInfo;
+
+public class Car {
+
+    String brand;
+    String model;
+    int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo(){
+
+       return String.format("The car is: %s %s - %d HP.", this.getBrand(), this.getModel(), this.getHorsePower());
+    }
+
+}
