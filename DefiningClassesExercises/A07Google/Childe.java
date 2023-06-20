8e5571846ae05a834bbb0068225373d3271f6932
IvanRusenov <i_rusenov@abv.bg>
2023-06-20 06:37:50 +0200

    Create Childe.java

---
+++ DefiningClassesExercises/A07Google/Childe.java
@@ -0,0 +1,28 @@
+package DefiningClassesExercises.A07Google;
+
+public class Childe {
+
+    private String childName;
+    private String childBirthday;
+
+    public Childe(String childName, String childBirthday) {
+        this.childName = childName;
+        this.childBirthday = childBirthday;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public String getChildBirthday() {
+        return childBirthday;
+    }
+
+    public void setChildBirthday(String childBirthday) {
+        this.childBirthday = childBirthday;
+    }
+}
