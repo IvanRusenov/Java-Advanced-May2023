30674b80032e8d1ca24e86596c8a8d0eb3f6ec1d
IvanRusenov <i_rusenov@abv.bg>
2023-06-20 06:38:58 +0200

    Create Parent.java

---
+++ DefiningClassesExercises/A07Google/Parent.java
@@ -0,0 +1,33 @@
+package DefiningClassesExercises.A07Google;
+
+public class Parent {
+
+    private String parentName;
+    private String parentBirthday;
+
+    public Parent(String parentName, String parentBirthday) {
+        this.parentName = parentName;
+        this.parentBirthday = parentBirthday;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentBirthday() {
+        return parentBirthday;
+    }
+
+    public void setParentBirthday(String parentBirthday) {
+        this.parentBirthday = parentBirthday;
+    }
+
+    @Override
+    public  String toString(){
+        return String.format("%s %s\n",this.parentName, this.parentBirthday);
+    }
+}
