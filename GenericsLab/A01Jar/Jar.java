fe5b59ab7cc1c642f467164dc2d137d3741faea0
IvanRusenov <i_rusenov@abv.bg>
2023-07-01 07:29:14 +0200

    Create Jar.java

---
+++ GenericsLab/A01Jar/Jar.java
@@ -0,0 +1,21 @@
+package GenericsLab.A01Jar;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+
+    private ArrayDeque <T> stack;
+
+
+    public Jar() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(T element){
+        stack.push(element);
+    }
+
+    public T remove(){
+       return stack.pop();
+    }
+}
