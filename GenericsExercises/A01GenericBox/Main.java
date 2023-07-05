9dee193db9aa1ae3181fa8694ae050e66cc8e862
IvanRusenov <i_rusenov@abv.bg>
2023-07-05 07:35:53 +0200

    Create Main.java

---
+++ GenericsExercises/A01GenericBox/Main.java
@@ -0,0 +1,17 @@
+package GenericsExercises.A01GenericBox;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            Integer info = Integer.parseInt(scan.nextLine());
+            Box<Integer> box = new Box<>(info);
+            System.out.println(box);
+        }
+    }
+}
