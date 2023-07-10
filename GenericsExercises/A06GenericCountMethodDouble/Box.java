dfd9cd2fbde9d7d706d2e41cae00a4651504aaed
IvanRusenov <i_rusenov@abv.bg>
2023-07-10 09:09:51 +0200

    Create Box.java

---
+++ GenericsExercises/A06GenericCountMethodDouble/Box.java
@@ -0,0 +1,25 @@
+package GenericsExercises.A06GenericCountMethodDouble;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Box <T extends Comparable<T>>{
+
+    private List<T> list;
+
+    public Box() {
+       list = new ArrayList<>();
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void swap(int index1, int index2){
+
+        Collections.swap(this.list,index1,index2);
+
+    }
+
+}
