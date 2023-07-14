8ef24f8ae98ea55ef4765bc470647bd1df948b50
IvanRusenov <i_rusenov@abv.bg>
2023-07-14 07:39:19 +0200

    Create Book.java

---
+++ IteratorsAndComparatorsLab/A02Library/Book.java
@@ -0,0 +1,45 @@
+package IteratorsAndComparatorsLab.A02Library;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String... authors) {
+
+        if (authors.length > 0) {
+            this.authors = Arrays.asList(authors);
+        }
+    }
+
+}
