0b69c097c869b5b026b2edad7df2f8696725bb9a
IvanRusenov <i_rusenov@abv.bg>
2023-07-23 06:43:37 +0200

    Create CompareByAge.java

---
+++ IteratorsAndComparatorsExercises/A06StrategyPattern/CompareByAge.java
@@ -0,0 +1,10 @@
+package IteratorsAndComparatorsExercises.A06StrategyPattern;
+
+import java.util.Comparator;
+
+public class CompareByAge implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}
