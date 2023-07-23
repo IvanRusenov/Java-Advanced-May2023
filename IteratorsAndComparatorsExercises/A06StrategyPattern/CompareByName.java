af39b3ea0bcc708560967f6fc9918672ba835db1
IvanRusenov <i_rusenov@abv.bg>
2023-07-23 06:44:23 +0200

    Create CompareByName.java

---
+++ IteratorsAndComparatorsExercises/A06StrategyPattern/CompareByName.java
@@ -0,0 +1,18 @@
+package IteratorsAndComparatorsExercises.A06StrategyPattern;
+
+import java.util.Comparator;
+
+public class CompareByName implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+
+        int result = Integer.compare(o1.getName().length(), o2.getName().length());
+
+        if (result == 0) {
+
+            return Integer.compare(o1.getName().toLowerCase().charAt(0), o2.getName().toLowerCase().charAt(0));
+        }
+
+        return result;
+    }
+}
