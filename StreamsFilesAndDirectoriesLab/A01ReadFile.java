5333077ea6498273f45701e7c24bc123e5ce4d61
IvanRusenov <i_rusenov@abv.bg>
2023-07-24 07:57:59 +0200

    Create A01ReadFile.java

---
+++ StreamsFilesAndDirectoriesLab/A01ReadFile.java
@@ -0,0 +1,34 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class A01ReadFile {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String filePath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+
+            int firstByte = inputStream.read();
+
+            while (firstByte >= 0){
+
+                System.out.printf("%s ", Integer.toBinaryString(firstByte));
+                firstByte= inputStream.read();
+
+            }
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
