f2d1f91206c7c66ebde7a0440084ecc2ad0c72de
IvanRusenov <114363124+IvanRusenov@users.noreply.github.com>
2023-07-25 06:31:07 +0200

    Update A02WriteToFile.java

--- StreamsFilesAndDirectoriesLab/A02WriteToFile.java
+++ StreamsFilesAndDirectoriesLab/A02WriteToFile.java
@@ -10,7 +10,7 @@ public class A02WriteToFile {
 
     public static void main(String[] args) {
 
-        String filePath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
+        String filePath = "C:\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
 
         String readPath = filePath + "\\input.txt";
         String writePath = filePath + "\\output.txt";
