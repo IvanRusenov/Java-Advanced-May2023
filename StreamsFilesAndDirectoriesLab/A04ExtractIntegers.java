29fc03b39f63f6bf779663f1141c97ed1ed082c0
IvanRusenov <i_rusenov@abv.bg>
2023-07-27 09:15:50 +0200

    Create A04ExtractIntegers.java

---
+++ StreamsFilesAndDirectoriesLab/A04ExtractIntegers.java
@@ -0,0 +1,30 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class A04ExtractIntegers {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String filePath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
+        String readPath = filePath + "\\input.txt";
+        String writePath = filePath + "\\output1.txt";
+
+        Scanner reader = new Scanner(new FileInputStream(readPath));
+        PrintStream writer = new PrintStream(new FileOutputStream(writePath));
+
+        while (reader.hasNext()) {
+
+            if (reader.hasNextInt()) {
+
+                writer.println(reader.nextInt());
+
+            }
+
+            reader.next();
+
+        }
+
+    }
+}
