84176a41d98c8516b48996aa013ff98329480881
IvanRusenov <i_rusenov@abv.bg>
2023-08-02 07:51:23 +0200

    Create A05WriteEveryThirdLine.java

---
+++ StreamsFilesAndDirectoriesLab/A05WriteEveryThirdLine.java
@@ -0,0 +1,42 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.*;
+
+public class A05WriteEveryThirdLine {
+
+    public static void main(String[] args){
+
+        String filePath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
+        String readPath = filePath + "\\input.txt";
+        String writePath = filePath + "\\output1.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(readPath));
+
+            PrintStream write = new PrintStream(writePath)){
+
+            String line = reader.readLine();
+
+            int count = 0;
+
+            while (line != null){
+
+                count++;
+
+                if (count % 3 == 0){
+
+                    write.println(line);
+
+                }
+
+                line = reader.readLine();
+
+            }
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
