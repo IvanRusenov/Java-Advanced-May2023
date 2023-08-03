a7d64aebdb944a9fd5a74f2e56bc2c96f2f13ab2
IvanRusenov <i_rusenov@abv.bg>
2023-08-03 18:44:18 +0200

    Create A06SortLines.java

---
+++ StreamsFilesAndDirectoriesLab/A06SortLines.java
@@ -0,0 +1,24 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class A06SortLines {
+
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources";
+        String readPath = filePath + "\\input.txt";
+        String writePath = filePath + "\\output1.txt";
+        Path inputPath = Paths.get(readPath);
+        Path outputPath = Paths.get(writePath);
+
+        List<String> allLines = Files.readAllLines(inputPath).stream().sorted().collect(Collectors.toList());
+        Files.write(outputPath, allLines);
+
+    }
+}
