558e13ca092d469509f75ee361f0ead7531e9e56
IvanRusenov <i_rusenov@abv.bg>
2023-08-05 07:02:41 +0200

    Create A01SumLines.java

---
+++ StreamsFilesAndDirectoriesExercises/A01SumLines.java
@@ -0,0 +1,30 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class A01SumLines {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        List<String> lines = Files.readAllLines(Path.of(path));
+
+        lines.forEach(line -> {
+
+            int sum =0;
+
+            for (int i = 0; i < line.length(); i++) {
+
+                sum += line.charAt(i);
+
+            }
+
+            System.out.println(sum);
+
+        });
+
+    }
+}
