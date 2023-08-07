6c972d506e7e582c8de853071c97ab31a1df3369
IvanRusenov <i_rusenov@abv.bg>
2023-08-07 09:50:47 +0200

    Create A03AllCapitals.java

---
+++ StreamsFilesAndDirectoriesExercises/A03AllCapitals.java
@@ -0,0 +1,31 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class A03AllCapitals {
+
+    public static void main(String[] args) throws IOException {
+
+        String inPath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        String outPath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
+        OutputStream writer =  Files.newOutputStream(Path.of(outPath));
+
+        for (byte b : Files.readAllBytes(Path.of(inPath))) {
+
+            if (b >= 97 && b <= 122){
+
+                writer.write(b-32);
+                continue;
+
+            }
+
+                writer.write(b);
+
+        }
+
+    }
+}
