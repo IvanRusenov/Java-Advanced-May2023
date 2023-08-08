40a39da2ae2222c906e6b864411a69d97ae011fd
IvanRusenov <114363124+IvanRusenov@users.noreply.github.com>
2023-08-08 08:37:53 +0200

    Update A04CountCharacterTypes.java

--- StreamsFilesAndDirectoriesExercises/A04CountCharacterTypes.java
+++ StreamsFilesAndDirectoriesExercises/A04CountCharacterTypes.java
@@ -8,8 +8,8 @@ public class A04CountCharacterTypes {
 
     public static void main(String[] args) throws IOException {
 
-        String inPath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
-        String outPath = "C:\\Users\\Chimi\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
+        String inPath = "C:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        String outPath = "C:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
 
         try (BufferedReader reader = Files.newBufferedReader(Path.of(inPath));
              PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Path.of(outPath)))) {
