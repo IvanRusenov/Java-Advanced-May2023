6843e89035f153c9fa6404c44325b5d5faa9e1eb
IvanRusenov <i_rusenov@abv.bg>
2023-08-09 07:54:17 +0200

    Create A05LineNumbers.java

---
+++ StreamsFilesAndDirectoriesExercises/A05LineNumbers.java
@@ -0,0 +1,41 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class A05LineNumbers {
+
+    public static void main(String[] args) throws IOException {
+
+        String inPath = "C:\\Users\\Chimi\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputLineNumbers.txt";
+
+        String outPath = "C:\\Users\\Chimi\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
+
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(inPath));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(outPath))) {
+
+            String line = reader.readLine();
+            int counter = 1;
+
+            while (line != null){
+
+                String output = String.format("%d. %s\n", counter, line);
+
+                writer.write(output);
+                counter++;
+
+                line = reader.readLine();
+            }
+
+
+        }
+
+
+    }
+}
