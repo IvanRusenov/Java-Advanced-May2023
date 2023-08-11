67bc4292679a987086fb913d5f3326cfaead7b33
IvanRusenov <i_rusenov@abv.bg>
2023-08-11 10:34:52 +0200

    Create A07MergeTwoFiles.java

---
+++ StreamsFilesAndDirectoriesExercises/A07MergeTwoFiles.java
@@ -0,0 +1,50 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class A07MergeTwoFiles {
+
+    public static void main(String[] args) {
+
+        String inPath = "C:\\Users\\Chimi\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputOne.txt";
+
+        String inPath2 = "C:\\Users\\Chimi\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt";
+
+        String outPath = "C:\\Users\\Chimi\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\results.txt";
+
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(inPath));
+             BufferedReader reader2 = Files.newBufferedReader(Path.of(inPath2));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(outPath))) {
+
+            String line = reader.readLine();
+
+            while (line != null) {
+
+                writer.write(line + System.lineSeparator());
+                line = reader.readLine();
+
+            }
+
+            line = reader2.readLine();
+
+            while (line != null) {
+
+                writer.write(line + System.lineSeparator());
+                line = reader2.readLine();
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
